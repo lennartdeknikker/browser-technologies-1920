39c5f14355010e89f03e43caf3c3c27fa2b17b2e
lennartdeknikker <me@lenn4rt.com>
2020-03-18 14:22:25 +0100

    add basic interface

--- docs/routes/answer.js
+++ docs/routes/answer.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Poll Creator' });
+  res.send(req.query.code);
 });
 
 module.exports = router;
