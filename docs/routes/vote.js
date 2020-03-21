e01da2dffe5116b5494e92e0111c39fcd7c523f5
lennartdeknikker <me@lenn4rt.com>
2020-03-21 18:43:22 +0100

    add linter

--- docs/routes/vote.js
+++ docs/routes/vote.js
@@ -1,36 +1,35 @@
-var express = require('express');
-var router = express.Router();
+var express = require("express")
+var router = express.Router()
 
-const mongo = require('../mongo');
-const Poll = require('../pollmodel')
+const mongo = require("../mongo")
+const Poll = require("../pollmodel")
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  const vote = req.query.answer;
-  const code = req.query.code;
+router.get("/", function(req, res) {
+    const vote = req.query.answer
+    const code = req.query.code
   
-  const onConnect = async function() {
-    const query = { code: code, open: true };
-    const poll = await Poll.findOneAndUpdate(query, { 
-        $inc: {
-          [`answer${vote}.votes`]: 1
+    const onConnect = async function() {
+        const query = { code: code, open: true }
+        const poll = await Poll.findOneAndUpdate(query, { 
+            $inc: {
+                [`answer${vote}.votes`]: 1
+            }
+        }, {new:true}, function(err) {
+            if (err) return console.error(err)
         }
-      }, {new:true}, function(err) {
-        if (err) return console.error(err); else {
-        }
-      }
-    )
+        )
     
-    if (poll) {
-      res.redirect(`/results?code=${code}&voted=${vote}`)
-    } else {
-      res.render('error', {message: 'this poll is closed'})
+        if (poll) {
+            res.redirect(`/results?code=${code}&voted=${vote}`)
+        } else {
+            res.render("error", {message: "this poll is closed"})
+        }
     }
-  }
 
-  mongo(onConnect)
+    mongo(onConnect)
   
 
-});
+})
 
-module.exports = router;
+module.exports = router
